7bda67a2f3beee3f210aa8bbb1db2b80faea1aa6
Dave Richardson <djradon@users.noreply.github.com>
2025-08-20 20:06:33 -0700

    fixed missing "--format esm" from some packages.json; exclude docs from from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
         '**/*.d.ts',
         '**/*.config.*',
         '**/test/**',
-        '**/.next/**'
+        '**/.next/**',
+        '**/docs/**'
       ]
     }
   },
